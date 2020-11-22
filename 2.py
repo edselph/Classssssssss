11dc7d583d835545d925cd40b59213595f782140
Edselph <edsel.harren@binus.ac.id>
2020-11-22 21:51:16 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,3 +1,4 @@
+import math
 class circle():
     def __init__(self,radius,color):
         self.radius = radius 
@@ -5,9 +6,21 @@ class circle():
 
     def GetRadius(self):
         return self.radius
-    
-    def SetRadius(self):
-        return self.GetRadius * 2.5
-    
-    
-    
+        
+    def GetColor(self):
+        return self.GetColor
+
+    def GetArea(self): 
+        return (self.radius**2)* math.pi
+        
+class cilinder(circle):
+    def __init__(self,height):
+        self.height = height
+
+    def Volume(self):
+        return(self.GetArea * self.height)
+
+
+Lingkaran = circle (2,"red")
+
+print(Lingkaran.cilinder)
