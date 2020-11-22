4c37292d2ba922334cda895631b8685faa762c79
Edselph <edsel.harren@binus.ac.id>
2020-11-22 21:16:09 +0700

    Initial commit

---
+++ 1.py
@@ -0,0 +1,14 @@
+class object:
+    pass
+class x(object):
+    pass
+class y(object):
+    pass
+class z(object):
+    pass
+class a(x,y):
+    pass
+class b(y,z):
+    pass
+class m(a,b):
+    pass
---
+++ 2.py
@@ -0,0 +1,13 @@
+class circle():
+    def __init__(self,radius,color):
+        self.radius = radius 
+        self.color = color
+
+    def GetRadius(self):
+        return self.radius
+    
+    def SetRadius(self):
+        return self.GetRadius * 2.5
+    
+    
+    
