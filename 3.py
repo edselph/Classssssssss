46fa539023265f345d6960230d3704dcc7f0179c
Edselph <edsel.harren@binus.ac.id>
2020-11-23 13:31:01 +0700

    pr 3 done(?)
    kelar i think(?)

--- 3.py
+++ 3.py
@@ -1,16 +1,48 @@
 class Person():
-    def __init__(self,name,address,numbercourse,course):
-        self.name = name
-        self.address = address
-        self.numbercourse = numbercourse
-        self.course = course
+    def __init__(self,name,address):
+        self.__name = name
+        self.__address = address
+    
+    def GetName(self):
+        return self.__name
+
+    def GetAddress(self):
+        return self.__address
+
+    def SetAddress(self,address):
+        self.__address = address
+
+    def __str__(self):
+        return self.GetName() + "(" + self.GetAddress() + ")"        
     
 class student(Person):
-    def __init__(self,CourseGrade):
-        self.CourseGrade = CourseGrade
+
+    def __init__(self,name,address,numcourse = 0,course =[],grade = []):
+        super().__init__(name, address)
+        self.__numcourse = numcourse
+        self.__course = course
+        self.__grade = grade
+
+
+    def addcoursegrade(self,course,grade):
+        self.__course.append(course)
+        self.__grade.append(grade)
+
+    def printgrade(self):
+        print(self.__grade)
 
     def Average():
-       return (self.CourseGrade/self.numbercourse)
+       return (sum(self.__grade)/len(self.grade))
 
-    super().__init__()
+class teacher(Person):
+    course = []
+    def addcoursegrade(self,course):
+        if self.course() not in course:
+            course.append(self.__course())
+        else:
+            print("There's already courses")
     
+    def removecourse(self,course):
+        if self.__course() in course:
+            course.remove(self.course())
+            return   
